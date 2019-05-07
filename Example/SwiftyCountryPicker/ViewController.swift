bfe7b597d4ebe00911d88d7ebaaae0aef4a664b1
anpkmr <anup227kumar@gmail.com>
2019-05-07 15:35:23 +0530

    Removed duplicate files

--- Example/SwiftyCountryPicker/ViewController.swift
+++ Example/SwiftyCountryPicker/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import SwiftyCountryPicker
 
 class ViewController: UIViewController {
-
+    
+//    @IBOutlet weak var pickerView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
